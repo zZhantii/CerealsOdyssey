36c31887b3937ccb4cb39a74bc91e41e662904bd
zZhantii <santiagoolozadab@gmail.com>
2024-11-07 11:15:13 +0100

    fixed

---
+++ proyecto/backend/views/main.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../frontend/css/style.css">
+    <link rel="shortcut icon" href="../../../frontend/img/Logo.png" type="image/x-icon">
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+    <title>Cereals Odyssey</title>
+</head>
+
+<body>
+    <main>
+        <section class="mx-5">
+            <div id="carouselExampleRide" class="carousel slide my-3" data-bs-ride="true">
+                <div class="carousel-inner">
+                    <div class="carousel-item active d-flex">
+                        <img src="../../frontend/img/banner1.png" class="d-block w-100 object-fit-cover" alt="Banner1" height="540px">
+                        <div class="square d-flex flex-column gap-4">
+                            <h1 class="text-white">Shop your favorite cereals</h1>
+                            <a href="#" class="btn btn-primary buttonMain2">Shop Celebrations</a>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </section>
+        <section class="mx-5 d-flex justify-content-center">
+            <div class="container filters my-3 d-flex justify-content-center">
+                <div class="row d-flex gap-4">
+                    <div class="col d-flex flex-column gap-2">
+                        <a href="#"><img src="../../frontend/img/photo1home.png" alt="filter1" height="262" width="262" class="object-fit-cover"></a>
+                        <h3>Classics</h3>
+                    </div>
+                    <div class="col d-flex flex-column gap-2">
+                        <a href="#"><img src="../../frontend/img/photo1home.png" alt="filter1" height="262" width="262" class="object-fit-cover"></a>
+                        <h3>Classics</h3>
+                    </div>
+                    <div class="col d-flex flex-column gap-2">
+                        <a href="#"><img src="../../frontend/img/photo1home.png" alt="filter1" height="262" width="262" class="object-fit-cover"></a>
+                        <h3>Classics</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="container d-flex justify-content-center ">
+            <div class="container m-5 p-0">
+                <div class="row productMain ps-5 d-flex align-items-center">
+                    <div class="col w-max py-5 px-0 d-flex gap-4 flex-column align-items-start">
+                        <h2>Celebrate with Odyssey</h2>
+                        <p>The perfect sweet snacks for your next party, or the perfect gift for your friends and family. Check out our cereal celebrations.</p>
+                        <a href="#" class="btn btn-primary buttonMain">Shop Celebrations</a>
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <img src="../../frontend/img/photo1home.png" alt="photo1home" height="380" width="380" class="object-fit-cover circle">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
